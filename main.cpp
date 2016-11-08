823219ae67d6e485b011b45949b216ccc3471916
TheHiddenDuke <jarand_ss@hotmail.com>
2016-11-08 13:39:14 +0100

    Complete

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<winsock2.h>
 #include<iostream>
+#include<thread>
 
 #pragma comment(lib,"ws2_32.lib") //Winsock Library
 
@@ -10,7 +11,7 @@ int main(int argc , char *argv[])
     SOCKET s;
     struct sockaddr_in server;
     char message[1024] , server_reply[2000];
-    int recv_size;
+    int recv_size, iresult;
 
     printf("\nInitialising Winsock...");
     if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
@@ -32,43 +33,54 @@ int main(int argc , char *argv[])
 
     server.sin_addr.s_addr = inet_addr("127.0.0.1");
     server.sin_family = AF_INET;
-    server.sin_port = htons( 27030 );
+    server.sin_port = htons( 8888 );
 
     //Connect to remote server
+
     if (connect(s , (struct sockaddr *)&server , sizeof(server)) < 0)
     {
         puts("connect error");
         return 1;
     }
 
-    puts("Connected");
+    std::cout << "Connected" << std::endl;
 
     //Send some data
 
     while(*message != 'q') {
-        std::cin.getline(message, 1024);
 
 
-        if (send(s, message, strlen(message), 0) < 0) {
-            puts("Send failed");
-            return 1;
-        }
-        //puts("Data Send\n");
+        std::thread tokimot([&] {
+            while(*message !='q') {
+                iresult = recv(s, server_reply, 2000, 0);
+                if (iresult == SOCKET_ERROR) {
+                    std::cout << "recv failed" << std::endl;
+                    return 1;
+                }
 
-        //Receive a reply from the server
-        if ((recv_size = recv(s, server_reply, 2000, 0)) == SOCKET_ERROR) {
-            puts("recv failed");
-        }
+                //Add a NULL terminating character to make it a proper string before printing
+                server_reply[iresult] = '\0';
+                std::cout << server_reply << std::endl;
+            }
+        });
 
-        puts("Reply received\n");
+        std::thread sender([&] {
+            while (*message!='q') {
+            std::cin.getline(message, 1024);
 
-        //Add a NULL terminating character to make it a proper string before printing
-        server_reply[recv_size] = '\0';
-        puts(server_reply);
+                iresult = send(s, message, strlen(message),0);
+                if (iresult < 0) {
+                    std::cout << "Send failed" << std::endl;
+                    return 1;
+                }
+            }
+        });
 
+        tokimot.join();
+        sender.join();
     }
 
-    closesocket(s);
+    shutdown(s, SD_SEND);
     WSACleanup();
     return 0;
 }
