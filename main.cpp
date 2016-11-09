c14316649ba24f37f941834d55ff4182c645a42f
TheHiddenDuke <jarand_ss@hotmail.com>
2016-11-09 13:14:17 +0100

    Complete

--- main.cpp
+++ main.cpp
@@ -9,11 +9,14 @@ int main(int argc , char *argv[])
 {
     WSADATA wsa;
     SOCKET s;
+
     struct sockaddr_in server;
+
     char message[1024] , server_reply[2000];
     int recv_size, iresult;
     bool run = true;
 
+    //starting up winsock
     printf("\nInitialising Winsock...");
     if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
     {
@@ -23,7 +26,7 @@ int main(int argc , char *argv[])
 
     printf("Initialised.\n");
 
-    //Create a socket
+    //Create a TCP socket
     if((s = socket(AF_INET , SOCK_STREAM , 0 )) == INVALID_SOCKET)
     {
         printf("Could not create socket : %d" , WSAGetLastError());
@@ -31,7 +34,7 @@ int main(int argc , char *argv[])
 
     printf("Socket created.\n");
 
-
+    //remote server IP, IPv4, port
     server.sin_addr.s_addr = inet_addr("127.0.0.1");
     server.sin_family = AF_INET;
     server.sin_port = htons( 8888 );
@@ -49,14 +52,16 @@ int main(int argc , char *argv[])
     //Send some data
 
     while(run) {
-
+        //Lambda thread for receiving data
         std::thread tokimot([&] {
             while(run) {
+                //iresult == 0 means no connection
                 iresult = recv(s, server_reply, 2000, 0);
                 if (iresult == SOCKET_ERROR) {
                     std::cout << "recv failed" << std::endl;
                     return 1;
                 }
+                //server shutdown sequence
                 if (iresult == 0){
                     std::cout << "Server shutdown..." << std::endl;
                     run = false;
@@ -70,7 +75,7 @@ int main(int argc , char *argv[])
                 std::cout << server_reply << std::endl;
             }
         });
-
+        //Lambda thread for sending data
         std::thread sender([&] {
             while (run) {
             std::cin.getline(message, 1024);
@@ -82,6 +87,7 @@ int main(int argc , char *argv[])
                         return 1;
                     }
                 }
+                    //Write letter 'q' to disconnect from server
                 else{
                     std::cout << "Disconnecting..." << std::endl;
                     run = false;
@@ -92,7 +98,7 @@ int main(int argc , char *argv[])
         tokimot.join();
         sender.join();
     }
-
+    //Shutdown sequence
     shutdown(s, SD_SEND);
     WSACleanup();
     return 0;
